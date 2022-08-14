762e6cafc3f36beacd0242c4a1184f57ecf0ca29
ksnx3684 <84880772+ksnx3684@users.noreply.github.com>
2022-08-14 17:09:06 +0900

    동적 웹페이지

--- main.js
+++ main.js
@@ -5,15 +5,36 @@ var url = require('url');
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
-    console.log(queryData.id);
+    var title = queryData.id;
     if(_url == '/'){
-      _url = '/index.html';
+      title = 'Welcome';
     }
     if(_url == '/favicon.ico'){
         return response.writeHead(404);
     }
     response.writeHead(200);
-    response.end(queryData.id);
+    var template = `
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+          <meta charset="UTF-8">
+          <meta http-equiv="X-UA-Compatible" content="IE=edge">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>${title}</title>
+      </head>
+      <body>
+          <ul>
+              <li><a href="/?id=html">html</a></li>
+          </ul>
+          <h1>no.1</h1>
+          <h2>${title}</h2>
+      </body>
+      </html>
+    `;
+    console.log(queryData.id);
+    // response.end(fs.readFileSync(__dirname + _url));
+    // response.end(queryData.id);
+    response.end(template);
  
 });
-app.listen(3000);
+app.listen(80);
